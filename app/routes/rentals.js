e831883a840dedd19a4ac3ee6cc9e44e9b0c63c1
Ace-Bansal <bansalekansh@gmail.com>
2018-12-08 20:47:11 +0530

    Commit from VSC

--- app/routes/rentals.js
+++ app/routes/rentals.js
@@ -1,4 +1,25 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+    model() {
+        return [
+            
+            {
+                id: 'First Mansion',
+                city: 'Dehradun',
+                price: 4500000
+            },
+            {
+                id: 'Second Casa',
+                city: 'Delhi',
+                price: 90000000
+            },
+            {
+                id: 'Third Adobe',
+                city: 'Mumbai',
+                price: 450000000
+            }
+        
+        ]
+    }
 });
